d393d34275db7ba9dc2f53e70cdb80c4d21c1328
Amethyst646 <141800409+Amethyst646@users.noreply.github.com>
2023-12-11 23:13:15 +0700

    Update test_util.py

--- cxplain/util/test_util.py
+++ cxplain/util/test_util.py
@@ -18,7 +18,7 @@ DEALINGS IN THE SOFTWARE.
 import numpy as np
 import tensorflow as tf
 from nose.tools import nottest
-from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.ensemble._forest import RandomForestClassifier
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
