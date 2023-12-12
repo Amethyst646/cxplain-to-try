1bbec77af2d5d3a2cea5f59a472f2382df0afe29
Amethyst646 <141800409+Amethyst646@users.noreply.github.com>
2023-12-12 18:27:22 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         "six >= 1.16.0",
         "scikit-learn >= 1.3.0",
-        "numpy >= 1.26.2",
+        "numpy >= 1.25.2",
         "scipy"
     ],
     tests_require=[
